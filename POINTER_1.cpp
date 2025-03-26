af306dc8b0200ce717b991fcc99a1075d9b48018
payalnandanwar <nandanwarpayal1@gmail.com>
2025-03-26 11:53:02 +0530

    Add files via upload
    1st Commit

---
+++ POINTER_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a = 100;
+    // int *b = &a;
+    // int **c = &b;
+    // cout << "Value of variable a is " << a << endl;
+    // cout << "Address of variable a is " << *b << endl;
+    // cout << "Address of pointer b is " << **c << endl;
+    // return 0;
+
+    int marks[] = {99, 100, 38};
+int *p = marks;
+cout << "The value of marks[0] is " << *p << endl;
+}
